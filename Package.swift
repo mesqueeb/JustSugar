51ec3ec849a00f84add1d2d03ea2ea141e83a9ed
mesqueeb <mesqueeb@users.noreply.github.com>
2024-07-10 17:43:36 +0900

    feat: enable strict concurrency

--- Package.swift
+++ Package.swift
@@ -5,17 +5,15 @@ import PackageDescription
 let package = Package(
   name: "JustSugar",
   products: [
-    // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(
       name: "JustSugar",
       targets: ["JustSugar"]
     ),
   ],
   targets: [
-    // Targets are the basic building blocks of a package, defining a module or a test suite.
-    // Targets can depend on other targets in this package and products from dependencies.
     .target(
-      name: "JustSugar"
+      name: "JustSugar",
+      swiftSettings: [.enableExperimentalFeature("StrictConcurrency")]
     ),
     .testTarget(
       name: "JustSugarTests",
